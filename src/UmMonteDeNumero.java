8a861272a4729328ff3e3f38dadb5cbf82febca5
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-04-01 21:20:08 -0300

    Criei um interface simples chamada "UmMonteDeNumero"

---
+++ src/UmMonteDeNumero.java
@@ -0,0 +1,19 @@
+public interface UmMonteDeNumero {
+    //"Contrato", uma promessa, uma ESPECIFICAÇÃO..
+
+    // Método para inserir um número NOVO....
+    public void inserir(int n);
+
+    // Método para buscar um número (verificar se ele existe)
+    public int buscar(int n);
+
+    // Método para me dizer o tamanho da estrutura de "um monte de número"
+    // Em bom mineirês: qual é o tamanho do "trem"?
+    public int tamanho();
+
+    // Método que ordena o "trem" ou esse "monte de número"
+    public void ordenar();
+
+    // Método que eu passo um "índice" e ele me devolve o número guardado no "trem"
+    public int numero(int index);
+}
