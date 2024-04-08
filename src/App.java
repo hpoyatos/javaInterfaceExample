dbbfb171423864eca7b44be58d7be472ff8fc3f1
Henrique Poyatos <hpoyatos@yahoo.com.br>
2024-04-08 19:56:51 -0300

    Testando a estrutura de Vetor...

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Usando a estrutura Vetor..
+        Vetor meuVetor = new Vetor(10);
+        System.out.println(meuVetor.tamanho());
+
+        meuVetor.inserir(20);
+        meuVetor.inserir(56);
+
+        System.out.println(meuVetor.buscar(56));
+        System.out.println(meuVetor.buscar(22));
+
+        System.out.println(meuVetor.numero(0));
+
+        System.out.println(meuVetor.numero(11));
+
+
     }
 }
